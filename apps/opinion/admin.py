5d18c72875f3c046b578ba5e43ca3d16d6c484a8
xakepanonim <dimka.fisenko@mail.ru>
2024-09-23 21:18:54 +0000

    opinion

--- apps/opinion/admin.py
+++ apps/opinion/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-# Register your models here.
+from apps.opinion.models import Opinion, OpinionComment, Grades
+
+
+@admin.register(Opinion)
+class OpinionAdmin(admin.ModelAdmin):
+    list_display = (
+        'uuid',
+        'user_display',
+        'product',
+        'total_likes',
+    )
+
+    def user_display(self, obj):
+        return format_html(
+            '<a href="{}">{}</a>',
+            obj.user.get_absolute_url(),
+            obj.user,
+        )
+
+    user_display.short_description = 'Пользователь'
+
+
+@admin.register(OpinionComment)
+class OpinionCommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'uuid',
+        'user_display',
+        'opinion',
+        'total_likes',
+    )
+
+    def user_display(self, obj):
+        return format_html(
+            '<a href="{}">{}</a>',
+            obj.user.get_absolute_url(),
+            obj.user,
+        )
+
+    user_display.short_description = 'Пользователь'
+
+
+@admin.register(Grades)
+class GradesAdmin(admin.ModelAdmin):
+    list_display = (
+        'uuid',
+        'title',
+        'grade',
+        'opinion',
+    )
