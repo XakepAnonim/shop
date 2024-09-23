fca034cb8cd1296b1eb6199e0cd92b25b8938495
DFisenko <dimka.fisenko@mail.ru>
2024-09-23 16:54:37 +0300

    cart

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
-from apps.cart.views import get_cart_handler
+from apps.cart.views import get_cart_handler, post_cart_handler
 
-urlpatterns = [path('', get_cart_handler)]
+urlpatterns = [
+    path('', get_cart_handler),
+    path('<uuid:uuid>/<slug:slug>/', post_cart_handler),
+]
